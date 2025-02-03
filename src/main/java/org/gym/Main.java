f20824d9fbc16f3dd69a0a941a639838bc0e0228
Paul Liebiediev <pel7373@gmail.com>
2025-02-04 01:04:28 +0200

    Transient

--- src/main/java/org/gym/Main.java
+++ src/main/java/org/gym/Main.java
@@ -3,10 +3,15 @@ package org.gym;
 import org.gym.config.Config;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class Main {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+//        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://regres.in/api/users/2";
+        String response = restTemplate.getForObject(url, String.class);
+        System.out.println(response);
     }
 }
--- src/main/java/org/gym/controller/TraineeController.java
+++ src/main/java/org/gym/controller/TraineeController.java
@@ -1,5 +1,6 @@
 package org.gym.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.gym.dto.TraineeDto;
 import org.gym.entity.Trainee;
 import org.gym.facade.TraineeFacade;
@@ -12,55 +13,56 @@ import java.awt.print.Book;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(value = "/books", consumes = {"application/JSON"},
         produces = {"application/JSON", "application/XML"})
 public class TraineeController {
-    @Autowired
-    private TraineeFacade traineeFacade;
 
-    @Autowired
-    private Convertor<TraineeDto, Trainee> convertor;
+    private final TraineeFacade traineeFacade;
 
-    @GetMapping
-    ResponseEntity<List<BookDTO>> getAll(
-            @RequestParam(defaultValue = "10", required = false) int limit,
-            @RequestParam(defaultValue = "5", required = false) int offset) {
-        List<Book> bookList = traineeFacade.getAllBooks(limit, offest);
-        List<BookDTO> bookDTOList = convertor.convertModelListToDtoList(bookList);
-        return new ResponseEntity<>(bookDTOList, HttpStatus.OK);
-    }
-
-    @GetMapping(value = "/{id:\\d+}")
-    ResponseEntity<List<BookDTO>> getById(@PathVariable long id) {
-        Book book = traineeFacade.getBookById(id);
-        BookDTO bookDTO = convertor.convertModelToDto(book);
-        return new ResponseEntity<>(bookDTO, HttpStatus.OK);
-    }
-
-    @PostMapping
-    ResponseEntity<BookDTO> create(@RequestBody BookCreateDTO bookCreateDTO) {
-        Book book = convertor.convertDtoToModel(bookCreateDTO);
-        Book createdBook = traineeFacade.createBook(book);
-        return new ResponseEntity<>(convertor.convertModelToDto(createdBook), HttpStatus.CREATE);
-    }
-
-    @PutMapping(value = "/{id:\\d+}")
-    ResponseEntity<BookDTO> update(@PathVariable long id, @Valid @RequestBody BookUpdateDTO bookUpdateDTO) {
-        Book book = convertor.convertDtoToModel(bookUpdateDTO);
-        Book updatedBook = traineeFacade.updateBook(book);
-        return new ResponseEntity<>(convertor.convertModelToDto(updatedBook), HttpStatus.OK);
-    }
-
-    @PatchMapping(value = "/{id:\\d+}")
-    ResponseEntity<BookDTO> patch(@PathVariable long id, @Valid @RequestBody BookPatchDTO bookPatchDTO) {
-        Book book = convertor.convertDtoToModel(bookPatchDTO);
-        Book patchedBook = traineeFacade.patchBook(id, book);
-        return new ResponseEntity<>(convertor.convertModelToDto(patchedBook), HttpStatus.OK);
-    }
-
-    @DeleteMapping(value = "/{id:\\d+}")
-    ResponseEntity<V> delete(@PathVariable long id) {
-        traineeFacade.deleteBok(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    }
+//    @Autowired
+//    private Convertor<TraineeDto, Trainee> convertor;
+//
+//    @GetMapping
+//    ResponseEntity<List<BookDTO>> getAll(
+//            @RequestParam(defaultValue = "10", required = false) int limit,
+//            @RequestParam(defaultValue = "5", required = false) int offset) {
+//        List<Book> bookList = traineeFacade.getAllBooks(limit, offest);
+//        List<BookDTO> bookDTOList = convertor.convertModelListToDtoList(bookList);
+//        return new ResponseEntity<>(bookDTOList, HttpStatus.OK);
+//    }
+//
+//    @GetMapping(value = "/{id:\\d+}")
+//    ResponseEntity<List<BookDTO>> getById(@PathVariable long id) {
+//        Book book = traineeFacade.getBookById(id);
+//        BookDTO bookDTO = convertor.convertModelToDto(book);
+//        return new ResponseEntity<>(bookDTO, HttpStatus.OK);
+//    }
+//
+//    @PostMapping
+//    ResponseEntity<BookDTO> create(@RequestBody BookCreateDTO bookCreateDTO) {
+//        Book book = convertor.convertDtoToModel(bookCreateDTO);
+//        Book createdBook = traineeFacade.createBook(book);
+//        return new ResponseEntity<>(convertor.convertModelToDto(createdBook), HttpStatus.CREATE);
+//    }
+//
+//    @PutMapping(value = "/{id:\\d+}")
+//    ResponseEntity<BookDTO> update(@PathVariable long id, @Valid @RequestBody BookUpdateDTO bookUpdateDTO) {
+//        Book book = convertor.convertDtoToModel(bookUpdateDTO);
+//        Book updatedBook = traineeFacade.updateBook(book);
+//        return new ResponseEntity<>(convertor.convertModelToDto(updatedBook), HttpStatus.OK);
+//    }
+//
+//    @PatchMapping(value = "/{id:\\d+}")
+//    ResponseEntity<BookDTO> patch(@PathVariable long id, @Valid @RequestBody BookPatchDTO bookPatchDTO) {
+//        Book book = convertor.convertDtoToModel(bookPatchDTO);
+//        Book patchedBook = traineeFacade.patchBook(id, book);
+//        return new ResponseEntity<>(convertor.convertModelToDto(patchedBook), HttpStatus.OK);
+//    }
+//
+//    @DeleteMapping(value = "/{id:\\d+}")
+//    ResponseEntity<V> delete(@PathVariable long id) {
+//        traineeFacade.deleteBok(id);
+//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    }
 }
